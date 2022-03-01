6fa3b02871af7aa0c0a74f1371e11ea0bb8003fb
Mateus Monteiro <mateusmonteirooliveira@gmail.com>
2022-03-01 10:42:04 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/mateus/course/entities/User.java
+++ src/main/java/br/com/mateus/course/entities/User.java
@@ -1,19 +1,27 @@
 package br.com.mateus.course.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
     //usar ctrl + D para duplicar a linha
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
-    public User (){
+    public User() {
     }
+
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
